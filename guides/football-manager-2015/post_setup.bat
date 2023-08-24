d542619f1bee5234f481a71109200bd3d7d25396
octobinary82 <142850415+octobinary82@users.noreply.github.com>
2023-08-23 22:34:51 -0300

    Update post_setup.bat

--- guides/football-manager-2015/post_setup.bat
+++ guides/football-manager-2015/post_setup.bat
@@ -10,8 +10,6 @@ for /f "tokens=1,2,3,4,5,6 delims=#" %%a in ('echo %SETUP_RUN_MESSAGE%') do (
     set param2=%%c
 )
 
-set DRIVE_LETTER=%param1%
-
 if "%success_result%" neq "true" (
     echo false#1#O setup não pôde ser executado: %param2%
     exit
@@ -19,7 +17,8 @@ if "%success_result%" neq "true" (
 
 rem -------------
 
-copy %param1%\Crack\* "c:\agd\footballmanager2015\Football Manager 2015" /Y
+rem O DRIVE_LETTER aqui é o mesmo definido pelo iso_mount.bat 
+copy %DRIVE_LETTER%\Crack\* "c:\agd\footballmanager2015\Football Manager 2015" /Y
 
 if %errorlevel% neq 0 (
     echo false#2#Os arquivos do ativador não puderam ser copiados
