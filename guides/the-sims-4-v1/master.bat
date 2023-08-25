d96666843b36cf44db0070eee2d1d7da864128e2
Nickolas Carlos <142850415+octobinary82@users.noreply.github.com>
2023-08-25 15:40:59 -0300

    Update master.bat

--- guides/the-sims-4-v1/master.bat
+++ guides/the-sims-4-v1/master.bat
@@ -3,7 +3,7 @@ setlocal
 
 set SETUP_ABSOLUTE_EXECUTABLE_PATH=%1
 
-%SETUP_ABSOLUTE_EXECUTABLE_PATH% /FORCECLOSEAPPLICATIONS /SILENT /DIR=c:\agd\thesims4v1 >nul 2>&1
+%SETUP_ABSOLUTE_EXECUTABLE_PATH% /FORCECLOSEAPPLICATIONS /SILENT /DIR=d:\agd\thesims4v1 >nul 2>&1
 if %errorlevel% neq 0 (
 	echo false#1#O setup não pôde completar com êxito
 	exit
